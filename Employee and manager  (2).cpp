084dedad0ff000557114a2f0b90790f8c42f4b88
udayrayavarapu5 <udayrayavarapu5@gmail.com>
2026-01-30 19:39:55 +0530

    Add files via upload

---
+++ Employee and manager  (2).cpp
@@ -0,0 +1,58 @@
+// Online C++ compiler to run C++ program online#include<iostream>
+#include<string>
+#include<iostream>
+using namespace std;
+class Employee{
+    public:
+          string name;
+         int id;
+        float basicsalary;
+        float totalsalary;
+        void getEmployeeDetails()
+        {
+            cout<<"name:"<<endl;
+            cin>>name;
+            cout<<"id:"<<endl;
+            cin>>id;
+            cout<<"basicsalary:"<<endl;
+            cin>>basicsalary;
+        }
+        void calculatesalary()
+        {
+            totalsalary=basicsalary;
+        }
+        void displayEmployeeDetails()
+        {
+             cout<<"\nname:"<<name<<endl;
+            cout<<"id:"<<id<<endl;
+            cout<<"basicsalary:"<<basicsalary<<endl;
+             cout<<"totalsalary:"<<totalsalary<<endl;
+        }
+};
+class Manager:public Employee{
+    public:
+        int empallowence;
+        float calculates;
+        void getManagerDetails()
+        {
+             cout<<"empallowence:"<<endl;
+             cin>>empallowence;
+        }
+        void calculatesalary()
+        {
+            totalsalary=basicsalary+empallowence;
+        }
+        void displayManagerDetails()
+        {
+           displayEmployeeDetails();
+        }
+    };
+    int main()
+    {
+        Manager  m1;
+        m1.getManagerDetails();
+        m1.getEmployeeDetails();
+        m1.calculatesalary();
+        m1.displayManagerDetails();
+        return 0;
+    }
