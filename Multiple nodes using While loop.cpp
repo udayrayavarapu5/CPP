1be1626d406c1761e64853c26bfa3643a7e1a0a6
udayrayavarapu5 <udayrayavarapu5@gmail.com>
2026-03-01 12:20:14 +0530

    Add files via upload

---
+++ Multiple nodes using While loop.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+struct node{
+	int data;
+	node* next;
+};
+class LinkedList{
+	public:
+		node* head;
+		LinkedList()
+		{
+			head=NULL;
+		}
+		void insertionAtEnd(int value)
+		{
+			node* newnode=new node;
+			newnode->data=value;
+			newnode->next=NULL;
+			
+			if(head==NULL)
+			{
+				head=newnode;
+				return;
+			}
+			node* temp=head;
+				while(temp->next!=NULL)
+		 	{
+		 		temp=temp->next;
+			 }
+			temp->next=newnode;
+			
+		 } 
+		 void display()
+		 {
+		 	node* temp=head;
+		 	while(temp!=NULL)
+		 	{
+		 		cout<<(temp->data)<<"->";
+		 		temp=temp->next;
+			 }
+			 cout<<"koushik\n";
+		 }
+};
+int main()
+{
+	LinkedList list;
+	int n,value;
+	cout<<"enter no. of nodes:";
+	cin>>n ;
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Enter data:";
+		cin>>value;
+		list.insertionAtEnd(value);
+		
+	}
+	
+
+	list.display();
+	return 0;
+}
